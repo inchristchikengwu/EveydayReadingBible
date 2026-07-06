f5b5d9a4effea150667dd7e3220fd32db925ee14
inchristchikengwu <inchrist.chikeng@gmail.com>
2026-07-06 23:35:42 +0800

    Delete christian-devotion-pwa directory

--- christian-devotion-pwa/sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME='daily-devotion-v1';
-const ASSETS=['/','/index.html','/manifest.webmanifest','/icons/icon-192.png','/icons/icon-512.png'];
-self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))));self.clients.claim();});
-self.addEventListener('fetch',event=>{event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone(); if(event.request.method==='GET') caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy)); return response;}).catch(()=>caches.match('/index.html'))));});
