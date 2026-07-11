a8986196f729bf29c5a20dea8b2d368405772e59
inchristchikengwu <inchrist.chikeng@gmail.com>
2026-07-11 12:59:11 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-const CACHE='devotion-noapi-v1'; const ASSETS=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
